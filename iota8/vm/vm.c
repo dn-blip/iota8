27831ef889f576da2fb165341661ec030642384e
dn-blip <nzekwe.da@wcs-g.com>
2025-01-13 12:02:25 -0700

    Update vm.c

--- iota8/vm/vm.c
+++ iota8/vm/vm.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
 
+#define OPC(i) ((i)>>12)
+#define DR(i) (((i)>>9)&0x7)
+#define SR1(i) (((i)>>6)&0x7)
+#define SR2(i) ((i)&0x7)
+#define FIMM(i) ((i>>5)&01)
+#define IMM(i) ((i)&0x1F)
+#define SEXTIMM(i) sext(IMM(i),5)
+#define FCND(i) (((i)>>9)&0x7)
+#define POFF(i) sext((i)&0x3F, 6)
+#define POFF9(i) sext((i)&0x1FF, 9)
+#define POFF11(i) sext((i)&0x7FF, 11)
+#define FL(i) (((i)>>11)&1)
+#define BR(i) (((i)>>6)&0x7)
+#define TRP(i) ((i)&0xFF)
